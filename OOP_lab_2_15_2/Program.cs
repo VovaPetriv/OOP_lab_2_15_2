324e53284ff1a9e204c7b3e5fac19be225ccb362
VovaPetriv <vova.petriv2002@gmail.com>
2020-04-26 16:43:46 +0300

    Add project files.

---
+++ OOP_lab_2_15_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_lab_2_15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double x = -6; x <= 3; x += 0.75)
+            {
+                double y = Math.Pow(x, 4) - 12 * x + Math.Exp(x);
+
+                Console.WriteLine("x = {0:f2} y = {1:f4}", x, y);
+            }
+        }
+    }
+}
